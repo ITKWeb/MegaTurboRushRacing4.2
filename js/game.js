4cb76dde7d8eba33a1755ccd0704b597db675b98
Thienan Lacape <thienan.lacape@itkweb.com>
2013-12-20 15:08:16 +0100

    merge

--- js/game.js
+++ js/game.js
@@ -1,10 +1,14 @@
 $(function() {
     var enemy = $('#vehiculeEnemy')[0];
     enemy.style.top = "0px";
+    enemy.style.left = "300px";
     var start = parseInt(enemy.style.top);
+    var voiture = document.getElementById('voiture');
+    voiture.style.top = "300px";
+    voiture.style.left = "0px";
     var interval = setInterval(function() {
 	start++;
-	collision_ui(enemy);
+	collision_ui(voiture, enemy);
 	enemy.style.top = start.toString() + "px";
 	if (start == window.innerHeight) {
 	    clearInterval(interval);
@@ -13,7 +17,21 @@ $(function() {
     }, timeInterval);
 });
 
-function collision_ui(enemy){
-    enemy.style.top;
+function collision_ui(voiture, enemy){
+    var point_voiture_x = parseInt(voiture.style.left);
+    var point_voiture_y = parseInt(voiture.style.top);
+    var point_enemy_x = parseInt(enemy.style.left);
+    var point_enemy_y = parseInt(enemy.style.top);
 
+    console.log("point_voiture_x = " + point_voiture_x.toString());    
+    console.log("point_voiture_y = " + point_voiture_y.toString());
+    console.log("point_enemy_x = " + point_enemy_x.toString());
+    console.log("point_enemy_y = " + point_enemy_y.toString());
+
+    if (((point_voiture_x > point_enemy_x) && (point_voiture_x < (point_enemy_x + carWidth))))
+	{
+	    if (((point_voiture_y > point_enemy_y) && (point_voiture_y < (point_enemy_y + carHeight)))) {
+    		alert("Collision !");
+	    }
+	}
 }
