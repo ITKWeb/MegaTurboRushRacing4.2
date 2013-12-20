2f898650ae4e61e894d33e14f7bd65df4dee0512
Damien Dijol <damien.dijol@ysoria.com>
2013-12-20 14:46:27 +0100

    lock deplacement voiture

--- js/Voiture.js
+++ js/Voiture.js
@@ -1,40 +1,38 @@
 $(function(){
-    //var voiture = document.getElementById("voiture");
     var voiture = $("#voiture")[0];
-    voiture.style.left = "300px";
+    voiture.style.left = (window.innerWidth / 2 - 25) + "px" ;
+    voiture.style.bottom = "20px";
 });
 
     var CODE_TOUCHE_GAUCHE = 37; //Keycode pour la touche gauche
     var CODE_TOUCHE_DROITE = 39; //Keycode pour la touche droite
-    var pos = 5;
-
-/*var onKeyLeft = function(event) {
-    if ( event.keyCode == CODE_TOUCHE_GAUCHE ) {
-        ALLER_GAUCHE= true;
-    } else if ( event.keyCode == CODE_TOUCHE_DROITE ) {
-        ALLER_DROITE = true;
-        alert("droite!!");
-    }
-}
- 
-var onKeyUp = function(event) {
-    if ( event.keyCode == CODE_TOUCHE_GAUCHE ) {
-        ALLER_GAUCHE = false;
-    } else if ( event.keyCode == CODE_TOUCHE_DROITE ) {
-        ALLER_DROITE = false;
-    }
-}
-*/
+    var CODE_TOUCHE_HAUT = 38; //Keycode pour la touche haut
+    var CODE_TOUCHE_BAS = 40; //Keycode pour la touche bas
+    var move = 10; //Nombre de pixels que l'on déplace la voiture
+    var horizontalCalc;
+    var road = 900;
 
 function MouvementPlayer(keyCode){
-    if(keyCode == CODE_TOUCHE_GAUCHE){
-        calc = parseInt(voiture.style.left) - pos;
+        if(keyCode == CODE_TOUCHE_GAUCHE){
+            if(parseInt(voiture.style.left) >= 0){
+                horizontalCalc = parseInt(voiture.style.left) - move;
+            }
+        }
+        else if(keyCode == CODE_TOUCHE_DROITE){
+            if(parseInt(voiture.style.left) <= 900){
+                horizontalCalc = parseInt(voiture.style.left) + move;
+            }
+        }
         
-    }
-    else if(keyCode == CODE_TOUCHE_DROITE){
-        calc = parseInt(voiture.style.left) + pos;
-    }
-    voiture.style.left = calc.toString() + "px";
+        voiture.style.left = horizontalCalc.toString() + "px";
+
+        /*if(keyCode == CODE_TOUCHE_HAUT){
+            verticalCalc = parseInt(voiture.style.bottom) + move;
+        }
+        else if(keyCode == CODE_TOUCHE_BAS){
+            verticalCalc = parseInt(voiture.style.bottom) - move;
+        }
+        voiture.style.bottom = verticalCalc.toString() + "px"; DEPLACEMENT VERTICAL*/
 }
 
 window.onkeydown = function(e){
