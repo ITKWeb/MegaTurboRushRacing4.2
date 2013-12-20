7df4f4cbd75c49a0e93e99385dc743f1fc8a5903
Thienan Lacape <thienan.lacape@itkweb.com>
2013-12-20 16:23:05 +0100

    toto

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ $(function() {
     voiture.style.top = (window.innerHeight - carHeight * 2).toString() + "px";
     voiture.style.left = "0px";
     var interval = setInterval(function() {
-	start += 5;
+	start += 3;
 	collision_ui(voiture, enemy);
 	enemy.style.top = start.toString() + "px";
 	if (start == window.innerHeight) {
@@ -24,16 +24,16 @@ function collision_ui(voiture, enemy){
     var point_voiture_x = parseInt(voiture.style.left);
     var point_voiture_y = parseInt(voiture.style.top);
 
-    if (((point_voiture_x => point_enemy_x) && (point_voiture_x <= (point_enemy_x + carWidth))))
+    if (((point_voiture_x > point_enemy_x) && (point_voiture_x < (point_enemy_x + carWidth))))
 	{
-	    if (((point_voiture_y => point_enemy_y) && (point_voiture_y <= (point_enemy_y + carHeight)))) {
+	    if (((point_voiture_y > point_enemy_y) && (point_voiture_y < (point_enemy_y + carHeight)))) {
     		//alert("Collision !");
 		window.location = "gameover.html";
 	    }
 	}
     else{
-	if(((point_enemy_x <= (point_voiture_x + carWidth)) && ((point_voiture_x + carWidth) <= (point_enemy_x + carWidth)))) {
-	    if (((point_enemy_y <= point_voiture_y) && (point_voiture_y <= (point_enemy_y + carHeight)))) {
+	if(((point_enemy_x < (point_voiture_x + carWidth)) && ((point_voiture_x + carWidth) < (point_enemy_x + carWidth)))) {
+	    if (((point_enemy_y < point_voiture_y) && (point_voiture_y < (point_enemy_y + carHeight)))) {
 		//alert("Collisition à droite !!!!");
 		window.location = "gameover.html";   
 	    }
